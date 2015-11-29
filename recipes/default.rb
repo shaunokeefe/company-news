a3edaf01a922852132ae4490b82801016b3b646e
shaunokeefe <shaun.okeefe.0@gmail.com>
2015-11-29 13:05:20 +0900

    basic install for tomcat

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,27 @@
 #
 node.default['java']['jdk_version'] = 7
 include_recipe 'java'
+
+node.default['tomcat']['base_version'] = 7
+# NOTE: (shauno) The Tomcat cookbook is designed to be used with
+# roles but we're using a wrapper cookbook. One side effect of this is
+# that if we update an attribute that is used to set subsequent attributes
+# (e.g. base_version) then all the attributes need to be manually updated
+# NOTE: (shauno) the below updates are based on the attributes for debian in the
+# Tomcat cookbooks attributes file
+node.default['tomcat']['base_instance'] = "tomcat#{node['tomcat']['base_version']}"
+node.default['tomcat']['packages'] = ["tomcat#{node['tomcat']['base_version']}"]
+node.default['tomcat']['deploy_manager_packages'] = ["tomcat#{node['tomcat']['base_version']}-admin"]
+
+node.default['tomcat']['user'] = "tomcat#{node['tomcat']['base_version']}"
+node.default['tomcat']['group'] = "tomcat#{node['tomcat']['base_version']}"
+node.default['tomcat']['home'] = "/usr/share/tomcat#{node['tomcat']['base_version']}"
+node.default['tomcat']['base'] = "/var/lib/tomcat#{node['tomcat']['base_version']}"
+node.default['tomcat']['config_dir'] = "/etc/tomcat#{node['tomcat']['base_version']}"
+node.default['tomcat']['log_dir'] = "/var/log/tomcat#{node['tomcat']['base_version']}"
+node.default['tomcat']['tmp_dir'] = "/tmp/tomcat#{node['tomcat']['base_version']}-tmp"
+node.default['tomcat']['work_dir'] = "/var/cache/tomcat#{node['tomcat']['base_version']}"
+node.default['tomcat']['context_dir'] = "#{node['tomcat']['config_dir']}/Catalina/localhost"
+node.default['tomcat']['webapp_dir'] = "/var/lib/tomcat#{node['tomcat']['base_version']}/webapps"
+node.default['tomcat']['java_options'] = '-Xmx128M -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses'
+include_recipe 'tomcat'
--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -11,4 +11,34 @@ describe 'company-news::default' do
       its(:stdout) { should match(/java version "1\.7/) }
     end
   end
+
+  describe 'should have tomcat installed' do
+    describe package('tomcat7') do
+      it { should be_installed.by('apt') }
+    end
+  end
+
+  describe 'should have tomcat service running' do
+    describe service('tomcat7') do
+      it { should be_running }
+    end
+  end
+
+  describe 'should have tomcat user and group' do
+    describe group('tomcat7') do
+      it { should exist }
+    end
+
+    describe user('tomcat7') do
+      it { should exist }
+      it { should belong_to_group 'tomcat7' }
+      it { should have_home_directory '/usr/share/tomcat7' }
+    end
+  end
+
+  describe 'should have tomcat listening on port 8080' do
+    describe port(8080) do
+        it { should be_listening.with('tcp') }
+    end
+  end
 end

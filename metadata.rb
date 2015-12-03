81d5ba69f4ca39c3cfc6662cf8dcfd3adc7f9de7
shaunokeefe <shaun.okeefe.0@gmail.com>
2015-12-03 12:40:12 +0900

    bump minor version number

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'shaun.okeefe.0@gmail.com'
 license          'MIT'
 description      'Installs and configures company-news'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 supports 'ubuntu'
 
